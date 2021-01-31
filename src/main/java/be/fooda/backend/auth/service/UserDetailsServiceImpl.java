77ee4660e52362193585dcf70430701b1ea3e315
yilmazchef <yilmaz@mail.be>
2021-01-31 14:33:58 +0100

    ready to test on cloud..

--- src/main/java/be/fooda/backend/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/be/fooda/backend/auth/service/UserDetailsServiceImpl.java
@@ -2,7 +2,6 @@ package be.fooda.backend.auth.service;
 
 import be.fooda.backend.auth.dao.UserRepository;
 import be.fooda.backend.auth.model.entity.UserEntity;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -18,11 +17,13 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service   // It has to be annotated with @Service.
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 public class UserDetailsServiceImpl implements UserDetailsService {
 
-    private final BCryptPasswordEncoder encoder;
-    private final UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder encoder;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
