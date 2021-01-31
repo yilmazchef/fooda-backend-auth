891f5ecdbaa728de77f2b7b61bf9e0cf0e6f5a3c
yilmazchef <yilmaz@mail.be>
2021-01-31 22:05:57 +0100

    repository methods are refactored.

--- src/main/java/be/fooda/backend/auth/dao/UserRepository.java
+++ src/main/java/be/fooda/backend/auth/dao/UserRepository.java
@@ -3,17 +3,12 @@ package be.fooda.backend.auth.dao;
 
 import be.fooda.backend.auth.model.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, UUID> {
-
-    Optional<UserEntity> findByLogin(String login);
 
     boolean existsByLogin(String login);
 
--- src/main/java/be/fooda/backend/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/be/fooda/backend/auth/service/UserDetailsServiceImpl.java
@@ -12,15 +12,14 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service   // It has to be annotated with @Service.
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
-    private BCryptPasswordEncoder encoder;
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
     private UserRepository userRepository;
@@ -28,28 +27,28 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        final Optional<UserEntity> oAuthenticatedUser = userRepository.findByLogin(username);
-
-        if (!oAuthenticatedUser.isPresent())
+        if (userRepository.existsByLogin(username))
             // If user not found. Throw this exception.
             throw new UsernameNotFoundException("Username: " + username + " not found");
 
-        if (oAuthenticatedUser.get().getIsAuthenticated().equals(Boolean.FALSE))
+        final UserEntity authenticatedUser = userRepository.getOneByLogin(username);
+
+        if (authenticatedUser.getIsAuthenticated().equals(Boolean.FALSE))
             throw new UsernameNotFoundException("Username: " + username + " is unauthorized.");
 
         // Remember that Spring needs roles to be in this format: "ROLE_" + userRole (i.e. "ROLE_ADMIN")
         // So, we need to set it to that format, so we can verify and compare roles (i.e. hasRole("ADMIN")).
-        List<GrantedAuthority> grantedAuthorities = oAuthenticatedUser.get()
+        Set<GrantedAuthority> grantedAuthorities = authenticatedUser
                 .getRoles()
                 .stream()
                 .map(userRole -> (GrantedAuthority) new SimpleGrantedAuthority("" + userRole))
-                .collect(Collectors.toList());
+                .collect(Collectors.toSet());
 
         // The "User" class is provided by Spring and represents a model class for user to be returned by UserDetailsService
         // And used by auth manager to verify and check user authentication.
         return new User(
-                oAuthenticatedUser.get().getLogin(),
-                encoder.encode(oAuthenticatedUser.get().getPassword()),
+                authenticatedUser.getLogin(),
+                passwordEncoder.encode(authenticatedUser.getPassword()),
                 grantedAuthorities
         );
 
